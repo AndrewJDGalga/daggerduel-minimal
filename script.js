363747832266b8ff6beae8df31377a45d32328d3
Andrew DGalga <ex.spake@gmail.com>
2024-02-14 14:07:43 -0600

    basic token generation.

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
+const tokenCount = 20;
+
 $("document").ready(()=>{
+    //generate tokens
+    $(()=>{
+        for(let i = 0; i < tokenCount; i++) {
+            $('#game--board_black-tokens').append('<img class="game-token" src="assets/blackBladeToken.svg" alt="Black Blade Face token."></li>');
+            $('#game--board_black-tokens').append('<img class="game-token" src="assets/blackBootToken.svg" alt="Black Boot Face token."></li>');
+            $('#game--board_black-tokens').append('<img class="game-token" src="assets/blackBlankToken.svg" alt="Black Blade Face token."></li>');
+
+            $('#game--board_white-tokens').append('<img class="game-token" src="assets/whiteBladeToken.svg" alt="White Blade Face token."></li>');
+            $('#game--board_white-tokens').append('<img class="game-token" src="assets/whiteBootToken.svg" alt="White Boot Face token."></li>');
+            $('#game--board_white-tokens').append('<img class="game-token" src="assets/whiteBlankToken.svg" alt="White Blade Face token."></li>');
+        }
+    })
+    //make all tokens draggable
     $(()=>{
         //$('#draggable').draggable();
         $('.game-token').draggable();
