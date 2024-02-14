71fb21a01fa6fc4538ceacdd74f8f19971daf2a5
Andrew DGalga <ex.spake@gmail.com>
2024-02-14 15:48:24 -0600

    small update to token generation boot and blade methods to allow for path modification later if needed.

--- script.js
+++ script.js
@@ -14,16 +14,17 @@ const generateTokens = (pageTarget, elementToGenerate, count) =>{
 $("document").ready(()=>{
     //generate tokens
     $(()=>{
-        generateTokens('#game--board_black-tokens_blades', '<img class="game-token" src="assets/blackBladeToken.svg" alt="Black Blade Face token."></li>', tokenCount);
-        generateTokens('#game--board_black-tokens_boots', '<img class="game-token" src="assets/blackBootToken.svg" alt="Black Boot Face token."></li>', tokenCount);
+        generateTokens('#game--board_black-tokens_blades', `<img class="game-token" src=${blackBladePath} alt="Black Blade Face token."></li>`, tokenCount);
+        generateTokens('#game--board_black-tokens_boots', `<img class="game-token" src=${blackBootPath} alt="Black Boot Face token."></li>`, tokenCount);
         generateTokens('#game--board_black-tokens_blank', '<img class="game-token" src="assets/blackBlankToken.svg" alt="Black Blade Face token."></li>', tokenCount);
 
-        generateTokens('#game--board_white-tokens_blades', '<img class="game-token" src="assets/whiteBladeToken.svg" alt="White Blade Face token."></li>', tokenCount);
-        generateTokens('#game--board_white-tokens_boots', '<img class="game-token" src="assets/whiteBootToken.svg" alt="White Boot Face token."></li>', tokenCount);
+        generateTokens('#game--board_white-tokens_blades', `<img class="game-token" src=${whiteBladePath} alt="White Blade Face token."></li>`, tokenCount);
+        generateTokens('#game--board_white-tokens_boots', `<img class="game-token" src=${whiteBootPath} alt="White Boot Face token."></li>`, tokenCount);
         generateTokens('#game--board_white-tokens_blank', '<img class="game-token" src="assets/whiteBlankToken.svg" alt="White Blade Face token."></li>', tokenCount);
         //set tokens as draggable by class
         $('.game-token').draggable();
-    })
+    });
+
     $('#player1-footing').on('click', ()=>{
         
         if($('#player1-footing').data('facing') === 'blade'){
@@ -35,13 +36,10 @@ $("document").ready(()=>{
         }
     });
     $('#player2-footing').on('click', ()=>{
-        
         if($('#player2-footing').data('facing') === 'blade'){
-            console.log('blade set');
             $('#player2-footing').data('facing', 'boot');
             $('#footing-img-white').attr('src',whiteBootPath);
         } else {
-            console.log('boot set');
             $('#player2-footing').data('facing', 'blade');
             $('#footing-img-white').attr('src',whiteBladePath);
         }
