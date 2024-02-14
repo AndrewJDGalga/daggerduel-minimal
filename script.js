e3e26822525d69ce7864bdf46801018b6215ab9d
Andrew DGalga <ex.spake@gmail.com>
2024-02-14 11:59:51 -0600

    jQuery data attribute change. Test to learn how to swap, since I'm already using jQuery and know how with vanilla JS.

--- script.js
+++ script.js
@@ -2,4 +2,16 @@ $("document").ready(()=>{
     $(()=>{
         $('#draggable').draggable();
     });
+    $('#player1-footing').on('click', ()=>{
+        
+        if($('#player1-footing').data('facing') === 'blade'){
+            console.log('blade set');
+            $('#player1-footing').data('facing', 'boot');
+        } else {
+            console.log('boot set');
+            $('#player1-footing').data('facing', 'blade');
+        }
+        
+        //console.log($('#player1-footing').data('facing'));
+    });
 });
