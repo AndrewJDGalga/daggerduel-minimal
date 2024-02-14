22beaa3acf6d3c85ee2e22b0a7f83b5fff5d154f
Andrew DGalga <ex.spake@gmail.com>
2024-02-14 14:16:34 -0600

    cleaned up token generation a bit, though 6 for loops is probably a bit much.

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
 const tokenCount = 20;
 
+const generateTokens = (pageTarget, elementToGenerate, count) =>{
+    for(let i = 0; i < count; i++) {
+        $(pageTarget).append(elementToGenerate);
+    }
+}
+
 $("document").ready(()=>{
     //generate tokens
     $(()=>{
+        generateTokens('#game--board_black-tokens', '<img class="game-token" src="assets/blackBladeToken.svg" alt="Black Blade Face token."></li>', tokenCount);
+        generateTokens('#game--board_black-tokens', '<img class="game-token" src="assets/blackBootToken.svg" alt="Black Boot Face token."></li>', tokenCount);
+        generateTokens('#game--board_black-tokens', '<img class="game-token" src="assets/blackBlankToken.svg" alt="Black Blade Face token."></li>', tokenCount);
+
+        generateTokens('#game--board_white-tokens', '<img class="game-token" src="assets/whiteBladeToken.svg" alt="White Blade Face token."></li>', tokenCount);
+        generateTokens('#game--board_white-tokens', '<img class="game-token" src="assets/whiteBootToken.svg" alt="White Boot Face token."></li>', tokenCount);
+        generateTokens('#game--board_white-tokens', '<img class="game-token" src="assets/whiteBlankToken.svg" alt="White Blade Face token."></li>', tokenCount);
+        //set tokens as draggable by class
+        $('.game-token').draggable();
+        /*
         for(let i = 0; i < tokenCount; i++) {
             $('#game--board_black-tokens').append('<img class="game-token" src="assets/blackBladeToken.svg" alt="Black Blade Face token."></li>');
             $('#game--board_black-tokens').append('<img class="game-token" src="assets/blackBootToken.svg" alt="Black Boot Face token."></li>');
@@ -12,12 +28,8 @@ $("document").ready(()=>{
             $('#game--board_white-tokens').append('<img class="game-token" src="assets/whiteBootToken.svg" alt="White Boot Face token."></li>');
             $('#game--board_white-tokens').append('<img class="game-token" src="assets/whiteBlankToken.svg" alt="White Blade Face token."></li>');
         }
+        */
     })
-    //make all tokens draggable
-    $(()=>{
-        //$('#draggable').draggable();
-        $('.game-token').draggable();
-    });
     $('#player1-footing').on('click', ()=>{
         
         if($('#player1-footing').data('facing') === 'blade'){
